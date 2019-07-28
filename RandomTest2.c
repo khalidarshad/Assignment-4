902c167afc879065ebb5a06d48f900ad4f851dcf
Abdulaziz Al-Janahi <52611310+khalidarshad@users.noreply.github.com>
2019-07-28 21:21:20 +0500

    Create RandomTest2.c

---
+++ RandomTest2.c
@@ -0,0 +1,15 @@
+case minion;
+	{
+		//draw player hands
+		for (i = 0; i < numPlayers; i++)
+		{
+			//initialize hand size to zero
+			state->handCount[i] = 0;
+			state->discardCount[i] = 0;
+			//draw 5 cards
+			// for (j = 0; j < 5; j++) 
+			// {
+			// drawCard(i, state);
+			// } 
+		break;
+	}
