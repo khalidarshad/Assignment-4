9e372f3c671f3bfe5821f2dd2a9d6fcd9c43f7b2
Abdulaziz Al-Janahi <52611310+khalidarshad@users.noreply.github.com>
2019-07-28 21:20:44 +0500

    Create RandomTest1.c

---
+++ RandomTest1.c
@@ -0,0 +1,20 @@
+int cardEfect(int cardName)
+{
+	string option;
+	cin >> option;
+	switch (option)
+		case baron;
+	{
+		//draw player hands
+		for (i = 0; i < numPlayers; i++)
+		{
+			//initialize hand size to zero
+			state->handCount[i] = 0;
+			state->discardCount[i] = 0;
+			//draw 5 cards
+			// for (j = 0; j < 5; j++) 
+			// {
+			// drawCard(i, state);
+			// } 
+		break;
+	}
